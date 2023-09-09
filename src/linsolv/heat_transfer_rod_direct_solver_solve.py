82e06c1abd6f2c36fda1d5bb3c6b0fd9e1f1c122
Ali Bozorgzadeh <61201050+abzrg@users.noreply.github.com>
2023-09-09 21:54:02 +0330

    linsolv so far

---
+++ src/linsolv/heat_transfer_rod_direct_solver_solve.py
@@ -0,0 +1,109 @@
+"""
+Using Numpy's direct numerical solver (numpy.linalg.solve) to solve the one
+dimensional steady state heat transfer in a rod with heat source applied to it.
+
+Equation:
+
+    d^T / dx^2  =  -1/k S(x)
+
+
+    - T   : temperature
+    - x   : position
+    - S(x): source term
+    - k   : ?
+
+NOTE: This code was adapted from Tony Saad's Numerical Method course with some
+modifications to meet my taste
+"""
+
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import diag
+from numpy.linalg import solve
+
+
+def source(x):
+    """Source term: S(x) == Normal Distribution"""
+    # Magnitude of the pulse
+    alpha = 0.1
+
+    # Standard deviation: controls how wide the distribution is
+    sigma = 0.1
+
+    # Length of the domain (kinda repeating myself, but it's ok for now)
+    L = 1
+
+    # Location of the maximum (in this case two-thrids from the left side of the domain)
+    icenter = 2.0 * L / 3.0
+
+    return alpha * np.exp(-(((x - icenter) / sigma) ** 2))
+
+
+def main() -> int:
+    # Number of grid points along the rod
+    # (chose a value no more than 1000, or you'll regret ;)
+    grid_size: int = 1000  # [1]
+
+    # Physical parameters
+    k = 1e-5  # [?]
+
+    # Domain parameters
+    domain_length = 1.0  # [m]
+    dx = domain_length / (grid_size - 1)  # [m]
+    x = np.linspace(0, domain_length, grid_size)  # [m]
+
+    # Boundary conditions
+    T_left = 300  # K (Kelvin)
+    T_right = 350  # K
+
+    #
+    # -- Building Solution Matrices
+    #
+
+    # Main diagnoal matrix
+    d = -2.0 * np.ones(grid_size)
+    d[0] = 1.0
+    d[-1] = 1.0
+
+    # First upper diagonal matrix
+    u = np.ones(grid_size - 1)
+    u[0] = 0.0
+
+    # First lower diagonal matrix
+    l = np.ones(grid_size - 1)
+    l[-1] = 0.0
+
+    # Coefficient matrix
+    # NOTE:
+    #   diag(v, k): Extract a diagonal or construct a diagonal array
+    #       - if `v` is a 1D array, return a 2D array with `v` on the `k`th
+    #       diagonal. if `v` is a 2D array, return a copy of its `k`th diagonal.
+    #       - Use `k>0` for diagonals above the main diagonal, and 'k<0' for
+    #       diagonals below the main diagonal
+    #       - returns a ndarray
+    A = diag(l, -1) + diag(d, 0) + diag(u, 1)
+
+    # Right hand side vector
+    b = -dx * dx * source(x) / k
+    b[0] = T_left
+    b[-1] = T_right
+
+    # Time the solution procss done by Numpy's `solve` function
+    tic = time.time()
+    T = solve(A, b)  # solve into `T` field
+    toc = time.time()
+    print(f"It took {toc - tic}s to solve the system with {grid_size} equations")
+
+    # Plot the temperature
+    T = np.reshape(T, [1, grid_size])
+    plt.imshow(T, cmap="inferno", aspect="auto")
+    plt.colorbar()
+    plt.show()
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
